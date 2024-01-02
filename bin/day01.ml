e7524b4a1b1972b4ba7d20dd4d882ba4bdbb9fb4
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-02 16:50:45 +0100

    feat: solve day 1, 1st star

--- bin/day01.ml
+++ bin/day01.ml
@@ -1,1 +1,29 @@
+let read_file (path: string): string = 
+        In_channel.with_open_bin path In_channel.input_all
+
+let lines: string -> string list = 
+        String.split_on_char '\n'
+
+let rec to_nums (input: string list): int list = match input with 
+        | [] -> [] 
+        | a :: tail -> (int_of_string a) :: (to_nums tail)
+
+let rec drop_last input = match input with 
+        | [] -> []
+        | [_] -> []
+        | a :: tail -> a :: (drop_last tail)
+
+let parse input = lines input 
+        |> drop_last
+        |> to_nums
+
+let rec solve input = match input with
+        | [] -> 0 
+        | a :: ((b :: _) as list) when a < b -> 1 + solve list
+        | _ :: tail -> solve tail
+
 let () = print_endline "Advent of Code, day 1"
+let () = read_file "inputs/input01.txt"
+        |> parse
+        |> solve
+        |> Printf.printf "*  %d\n"
